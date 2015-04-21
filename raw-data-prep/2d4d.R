0f7fab959eb2912357697ebe07c0a61278465498
Joe-Hilgard <jhilgard@gmail.com>
2015-04-20 19:11:19 -0500

    Fixed 2d4d code. Turned crossed-out values to 0 in Debrief_TG.

--- raw-data-prep/2d4d.R
+++ raw-data-prep/2d4d.R
@@ -1,10 +1,10 @@
 #combine 2d4d measurements and establish interrater reliability
 #written by HARD KREW on 2/1/2014 on a day of freezing rain
 
-require(xlsx)
-jul<-read.xlsx(file="raw-data-prep/raw_data/digits_JS.xlsx", 1)
-rac<-read.xlsx(file="raw-data-prep/raw_data/digits_RP.xlsx", 1)
-tay<-read.xlsx(file="raw-data-prep/raw_data/digits_TG.xlsx", 1)
+library(readxl); library(dplyr)
+jul<-read_excel("raw-data-prep/raw_data/digits_JS.xlsx", 1)
+rac<-read_excel("raw-data-prep/raw_data/digits_RP.xlsx", 1)
+tay<-read_excel("raw-data-prep/raw_data/digits_TG.xlsx", 1)
 # check colnames
 mean(colnames(jul) == colnames(rac) &
        colnames(jul) == colnames(tay)) #should equal 1
@@ -17,14 +17,6 @@ tay = tay[complete.cases(tay[,1:9]),]
 # How thorough is the double-coding?
 temp = unique(c(jul[,1], rac[,1], tay[,1]))
 table(temp %in% jul[,1], temp %in% rac[,1], temp %in% tay[,1])
-# jul & rac overlap, tay stands alone
-# some records are too exactly alike in jul and rac datasets
-temp = jul[,2:9] == rac[,2:9]
-temp = apply(temp, 1, FUN=mean)
-rac[as.logical(temp),1:9]
-# removing rac[1:49,], must've been copy-pasted from julian somehow
-# not sure what to make of 57, 59, 75, 108, 140, 142, 154
-rac = rac[-(1:49),]
 # rachel's errors: 77 84 148
 bad=c(77,84,148)
 rac[rac$Subno %in% bad,2:9] = NA
@@ -33,24 +25,28 @@ rac$R_index_length[rac$R_index_length < 100] = NA
 
 # IRR?
 icc = data.frame("Subno" = 1:450)
-names(rac) = paste(names(rac), "_R", sep="")
-names(tay) = paste(names(tay), "_T", sep="")
+rac1 = rac
+tay1 = tay
+names(rac1) = paste(names(rac1), "_R", sep="")
+names(tay1) = paste(names(tay1), "_T", sep="")
 icc = cbind(icc, jul[match(icc$Subno, jul$Subno),2:9])
-icc = cbind(icc, rac[match(icc$Subno, rac$Subno),2:9])
-icc = cbind(icc, tay[match(icc$Subno, tay$Subno),2:9])
-cor(icc[,-1], use="pairwise.complete")
+icc = cbind(icc, rac1[match(icc$Subno, rac1$Subno),2:9])
+icc = cbind(icc, tay1[match(icc$Subno, tay1$Subno),2:9])
+cor(icc[,-1], use="pairwise.complete") %>%
+  round(3)
 
 # Create averages
 require(reshape2)
-dat = rbind(jul, rac, tay); 
-dat = dat[,-10] # setting aside notes for now
-molten = melt(dat, id.vars = "Subno")
+datTemp = rbind(jul, rac, tay); 
+datTemp = datTemp[,-10] # setting aside notes for now
+molten = melt(datTemp, id.vars = "Subno")
 dat = dcast(molten, Subno ~ ... , fun.aggregate=mean, na.rm=T)
 dat$julNotes = jul$Notes[match(dat$Subno, jul$Subno)]
 dat$racNotes = rac$Notes[match(dat$Subno, rac$Subno)]
 dat$tayNotes = tay$Notes[match(dat$Subno, tay$Subno)]
-# how many coded? # I must've forgotten something in here, this isn't working
-dat$nCoders = table(dat$Subno)
+# how many coded? 
+t = table(datTemp$Subno)
+dat$nCoders = t[match(names(t), dat$Subno)]
 # who coded?
 # dat$Coders = paste ###
 
