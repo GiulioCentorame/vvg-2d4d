cf293f444bccc6670f4dfd485729e9d19653b135
Joe Hilgard <jhilgard@gmail.com>
2017-10-13 21:25:52 -0500

    Working WIP of baddata fix. -Replaces merge conflict w/ Hyunji's by-hand corrections.

---
+++ fix_baddata_wip.R
@@ -0,0 +1,95 @@
+# One way to implement the hyunji hand-corrected data into the flawed data
+# pound the data flat, replace dat$value with hyunji$value
+
+library(tidyverse)
+library(readxl)
+
+# Function for overwriting merge conflicts with real values
+force.numeric = function(x) {
+  x <- x[x != -999] # discard conflicts
+  # If all values are NA, return NA
+  output <- ifelse(length(na.omit(x)) == 0, as.numeric(NA),
+                   # otherwise, if all values are equal, merge them
+                   ifelse(length(unique(na.omit(x))) == 1, unique(x),
+                          # otherwise, return class-appropriate conflict code
+                          -999))
+  return(output)
+}
+
+# function for merging strings and flagging failures to match
+force.character = function(x) {
+  x <- x[x != "CONFLICT!"] # discard conflicts
+  # If all values are NA, return NA
+  output <- ifelse(length(na.omit(x)) == 0, as.character(NA),
+                   # otherwise, if all values are equal, merge them
+                   ifelse(length(unique(na.omit(x))) == 1, unique(x),
+                          # otherwise, return class-appropriate conflict code
+                          "CONFLICT!"))
+  return(output)
+}
+
+
+# demo
+dat <- data.frame("ID" = 1:4,
+                  "A" = c("red", "blue", "CONFLICT!", "green"),
+                  "B" = c(1, 3, 10, -999),
+                  stringsAsFactors = F)
+
+hyunji <- data.frame("ID" = 1:4,
+                     "A" = c(NA, NA, "red", NA),
+                     "B" = c(NA, NA, NA, 5),
+                     stringsAsFactors = F)
+
+(flatdat <- gather(dat, key, value, A:B))
+(flathyunji <- gather(hyunji, key, value, A:B))
+
+left_join(flatdat, flathyunji, by = c("ID", "key"))
+
+bind_rows(flatdat, flathyunji) %>% 
+  filter(key == "B") %>% 
+  group_by(ID, key) %>% 
+  summarize(value = force.numeric(value))
+
+bind_rows(flatdat, flathyunji) %>% 
+  filter(key == "A") %>% 
+  group_by(ID, key) %>% 
+  summarize(value = force.character(value))
+
+# ok for real now
+dat <- read.delim("clean_data.txt", stringsAsFactors = F)
+# hyunji labeled irreconcilably bad or missing data as "BAD"
+# I don't want to screw up my classes with character data so I'll treat that as NA
+hyunji <- read_excel("debug/master_baddata.xlsx", na = c("", "BAD"))
+
+# separate by class and flatten
+flatdat.num <- dat %>% 
+  group_by(Subject) %>%
+  select_if(is.numeric) %>% 
+  gather(key, value, -Subject)
+flatdat.chr <- dat %>% 
+  group_by(Subject) %>% 
+  select_if(is.character) %>% 
+  gather(key, value, -Subject)
+hyunji.num <- hyunji %>% 
+  group_by(Subject) %>%
+  select_if(is.numeric) %>% 
+  gather(key, value, -Subject)
+hyunji.chr <- hyunji %>% 
+  group_by(Subject) %>% 
+  select_if(is.character) %>% 
+  gather(key, value, -Subject)
+
+fused.num <- bind_rows(flatdat.num, hyunji.num) %>% 
+  group_by(Subject, key) %>% 
+  summarize(value = force.numeric(value))
+fused.chr <- bind_rows(flatdat.chr, hyunji.chr) %>% 
+  group_by(Subject, key) %>% 
+  summarize(value = force.character(value))
+
+wide.num <- spread(fused.num, key, value)
+wide.chr <- spread(fused.chr, key, value)
+fixed.dat <- full_join(wide.num, wide.chr, by = "Subject")
+
+# rearrange as the order it once was
+fixed.dat <- fixed.dat %>% 
+  select(names(dat))
